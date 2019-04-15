c37bd0d58cfd142fff8a01834f1675c06218d06a
Darren <darren.sequeira.ds@gmail.com>
2019-04-16 01:22:59 +0530

    Added Resolver

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
 
 declare let toastr;
 
@@ -18,15 +19,16 @@ declare let toastr;
     `
 })
 export class EventListComponent implements OnInit {
-    events: any[];
+    events: any;
 
     constructor(
         private eventService: EventService,
-        private toastrService: ToastrService
+        private toastrService: ToastrService,
+        private route: ActivatedRoute
     ) { }
 
     ngOnInit() {
-        this.events = this.eventService.getEvents();
+        this.events = this.route.snapshot.data['events'];
     }
 
     handleThumbnailClick(eventName) {
--- ng-fundamentals/src/app/routes.ts
+++ ng-fundamentals/src/app/routes.ts
@@ -4,9 +4,10 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouterActivator } from './events/event-details/event-route-activator.service';
+import { EventListResolver } from './events/events-list.resolver.service';
 
 export const appRoutes: Routes = [
-    { path: 'events', component: EventListComponent },
+    { path: 'events', component: EventListComponent, resolve: { events: EventListResolver } },
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouterActivator] },
     { path: '404', component: Error404Component },
